f8b2791e5271c5b63325fb99e7f748888f9baf13
MinayeAliyeva <7s346pu@code.edu.az>
2023-12-02 00:22:43 +0400

    changes

--- booking-app/src/routes/Routes.tsx
+++ booking-app/src/routes/Routes.tsx
@@ -1,22 +1,9 @@
-import { RouteObject } from "react-router-dom";
+
 import App from "../App";
 import About from "../components/about/About";
 import Home from "../components/home/Home";
 
-// interface Route{
-//     path: string;
-//     element: JSX.Element;
-//     children: ({
-//         index: boolean;
-//         element: JSX.Element;
-//         path?: undefined;
-//     } | {
-//         path: string;
-//         element: JSX.Element;
-//         index?: undefined;
-//     })[];
-// }
-export const routes:Route = [
+export const routes = [
   {
     path: "",
     element: <App />,
